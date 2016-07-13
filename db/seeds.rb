54142a3718adaa8e9fe1dfcdcb4945231f99d6c0
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-07-13 23:46:46 +0900

    add seed for test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = email.match(/(.*)@/)[1]
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: name
+               )
+end
+
+User.all.each do |user|
+
+  Blog.create! title: "title by " + user.name,
+               content: "content by " + user.name,
+               user_id: user.id
+end
