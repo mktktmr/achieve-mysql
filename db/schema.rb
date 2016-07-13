f6ff60913ac6baa89190f91f54456715facd3d1f
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-07-13 21:46:51 +0900

    add user_id column to Blogs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713113735) do
+ActiveRecord::Schema.define(version: 20160713124603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160713113735) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "contacts", force: :cascade do |t|
