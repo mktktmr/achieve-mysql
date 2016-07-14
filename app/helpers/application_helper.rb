8463e3a24cc7d9e9257dacc76842e0fc25f17abd
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-07-15 04:11:03 +0900

    バリデーションエラーのメッセージのデザイン変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,40 @@
 module ApplicationHelper
 end
+
+module ActionView
+  module Helpers
+    module FormHelper
+      def error_messages!(object_name, options = {})
+        resource = self.instance_variable_get("@#{object_name}")
+        return '' if !resource || resource.errors.empty?
+
+        messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+
+        html = <<-HTML
+          <div class="alert alert-danger">
+            <ul>#{messages}</ul>
+          </div>
+        HTML
+
+        html.html_safe
+      end
+
+      def error_css(object_name, method, options = {})
+        resource = self.instance_variable_get("@#{object_name}")
+        return '' if resource.errors.empty?
+
+        resource.errors.include?(method) ? 'has-error' : ''
+      end
+    end
+
+    class FormBuilder
+      def error_messages!(options = {})
+        @template.error_messages!(@object_name, options.merge(object: @object))
+      end
+
+      def error_css(method, options = {})
+        @template.error_css(@object_name, method, options.merge(object: @object))
+      end
+    end
+  end
+end
