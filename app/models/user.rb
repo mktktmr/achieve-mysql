e4bb02d50ee139579f9ece79d754712544c15f0a
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-07-15 23:56:18 +0900

    add omniauthable to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :confirmable, :omniauthable
 
   has_many :blogs
 
