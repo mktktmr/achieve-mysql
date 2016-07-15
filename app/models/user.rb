6a553c951a3434f0a1cd10ea7262942c6a10c1bf
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-07-16 02:46:38 +0900

    add oauth by twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,22 @@ class User < ActiveRecord::Base
     end
     user
   end
+
+  def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+
+    unless user
+      user = User.new(
+          name:     auth.info.nickname,
+          image_url: auth.info.image,
+          provider: auth.provider,
+          uid:      auth.uid,
+          email:    auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
+          password: Devise.friendly_token[0, 20],
+      )
+      user.skip_confirmation!
+      user.save
+    end
+    user
+  end
 end
