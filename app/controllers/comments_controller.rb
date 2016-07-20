79b92c747ef23ad795b43b5ed7991bfcee61dbbd
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-07-21 07:56:56 +0900

    add function to post comment by ajax.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.build(comment_params)
+    @blog = @comment.blog
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+        format.json { render :show, status: :created, location: @comment }
+        format.js { render :index }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:blog_id, :content)
+  end
+end
